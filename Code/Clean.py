4ed8fb6d0d5fb25fa0d7417c04c94a5db792abd1
Pablo Cano Carciofa <pcanocarciofa@gmail.com>
2022-12-22 21:53:28 +0000

    reading in

--- Code/Clean.py
+++ Code/Clean.py
@@ -1,1 +1,9 @@
+#Loading in packages
+import numpy as np
+import pandas as pd
+ratings = pd.read_csv("Data/BX-Book-Ratings.csv")
+books = pd.read_csv("Data/BX-Books.csv")
+users = pd.read_csv("Data/BX-Users.csv")
+
+print(ratings.shape)
 print("hello world")
