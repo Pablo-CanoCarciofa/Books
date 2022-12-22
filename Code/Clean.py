8de03fe1427ee7a0dcd7d75d93e2cc2385d84578
Pablo Cano Carciofa <pcanocarciofa@gmail.com>
2022-12-22 22:27:45 +0000

    reading in

--- Code/Clean.py
+++ Code/Clean.py
@@ -1,9 +1,7 @@
 #Loading in packages
 import numpy as np
 import pandas as pd
-ratings = pd.read_csv("Data/BX-Book-Ratings.csv")
-books = pd.read_csv("Data/BX-Books.csv")
-users = pd.read_csv("Data/BX-Users.csv")
-
-print(ratings.shape)
-print("hello world")
+ratings = pd.read_csv("Data\BX-Book-Ratings.csv", sep = ";", on_bad_lines = 'skip', encoding = "latin-1")
+books = pd.read_csv("Data\BX-Books.csv", sep = ";", on_bad_lines = 'skip', encoding = "latin-1")
+users = pd.read_csv("Data\BX-Users.csv", sep = ";", on_bad_lines = 'skip', encoding = "latin-1")
+print(books.head())
